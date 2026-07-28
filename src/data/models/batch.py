3c5935d661bc7a64f9824e1db978044b0963e683
Daniel <daniilkacuk@gmail.com>
2026-07-28 19:24:03 +0200

    fix: import models errors

--- src/data/models/batch.py
+++ src/data/models/batch.py
@@ -4,6 +4,9 @@ from src.core.database import Base
 
 from datetime import datetime
 
+from src.data.models.product import Product
+from src.data.models.workcenter import WorkCenter
+
 class Batch(Base):
     __tablename__ = 'batches'
 
