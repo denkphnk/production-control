6083fd5096fd84248023c9eeae67f17eaa49707d
Daniel <daniilkacuk@gmail.com>
2026-07-31 16:00:11 +0200

    AsyncSessionLocal

--- src/core/database.py
+++ src/core/database.py
@@ -10,12 +10,12 @@ engine = create_async_engine(
     echo=True
 )
 
-async_session = async_sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
     engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
 async def get_db():
-    async with async_session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
