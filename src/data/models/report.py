3ce43180575a67b1020b5004775b6051bc370eec
Daniel <daniilkacuk@gmail.com>
2026-08-08 14:35:57 +0200

    reports model

---
+++ src/data/models/report.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.core.database import Base
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    batch_id: Mapped[int] = mapped_column(
+        ForeignKey("batches.id")
+    )
+
+    file_name: Mapped[str]
+    file_path: Mapped[str]
+
+    status: Mapped[str]
+
+    created_at: Mapped[datetime]
